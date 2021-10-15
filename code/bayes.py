3fd07c9c5c2e3cd66e1c9d51ee1e3d92422a09b4
Krzysztof Rusek <krusek@agh.edu.pl>
2021-10-15 22:04:38 +0200

    Poczatek modelu Bayesowskiego

---
+++ code/bayes.py
@@ -0,0 +1,89 @@
+from dataclasses import dataclass
+from typing import Any, List, Tuple
+
+import pandas as pd
+from absl import flags, app, logging
+
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+import data
+
+tfd = tfp.distributions
+tfb = tfp.bijectors
+import numpy as np
+
+import gravity
+FLAGS = flags.FLAGS
+
+Root = tfd.JointDistributionCoroutine.Root
+
+
+def poisson_mixture_regression(x:Any,nnz:int=2, ):
+    @tfd.JointDistributionCoroutine
+    def model():
+        w = yield  Root(tfd.Sample(tfd.Normal(loc=0., scale=2.),nnz,name='w'))
+        c = yield Root(tfd.Sample(tfd.Normal(loc=0., scale=2.), 3,name='c'))
+        logits = yield Root(tfd.Sample(tfd.Normal(loc=tf.zeros(nnz+1), scale=4.),x.shape[0],name='logits'))
+
+        log_rate=x[...,tf.newaxis]@tf.concat([tf.convert_to_tensor([0.], dtype=tf.float32),w],axis=0)[tf.newaxis,...]+c
+        yield tfd.MixtureSameFamily(
+            mixture_distribution=tfd.Categorical(logits=tf.transpose(logits)),
+            components_distribution=tfd.Poisson(log_rate=log_rate),
+            name='y'
+        )
+    return model
+
+@dataclass
+class Dataset:
+    x:np.array
+    y:np.array
+    flat_idx:Tuple[np.array,np.array]
+    names: List[str]
+
+    @staticmethod
+    def from_pandas(df:pd.DataFrame, mass_type:gravity.CountryFeaturesType,dt=np.float32)->'Dataset':
+        A = np.array(df)
+        C = np.zeros((A.shape[1], A.shape[1]))
+        flat_idx = np.triu_indices(df.shape[1],k = 1)
+
+        for v in A:
+            v = np.sign(v)
+            M = v[..., np.newaxis] @ v[np.newaxis, ...]
+            M -= np.diag(np.diag(M))
+            C += M
+
+        if mass_type == gravity.CountryFeaturesType.ALL:
+            S = np.sum(A,axis=0)[..., np.newaxis]
+        elif mass_type == gravity.CountryFeaturesType.ONLY_COOPERATION:
+            S = A[(np.all(A != 1, axis=1)), :].sum(axis=0)[..., np.newaxis]
+        elif mass_type == gravity.CountryFeaturesType.INDIVIDUAL:
+            S = A[np.any(A == 1, axis=1), :].sum(axis=0)[..., np.newaxis]
+
+        y = C[flat_idx]
+        sst = S@S.T
+        x = sst[flat_idx]
+
+        where_x, = np.where(x)
+        dataset = Dataset(
+            x = np.log(x[where_x]),
+            y = y[where_x],
+            flat_idx = tuple(fi[where_x] for fi in flat_idx),
+            names=list(df.columns)
+        )
+        return dataset
+
+
+
+
+def main(_):
+    clean_df = data.load_clean(FLAGS.pickle)
+    df = data.fractions_countries(clean_df, with_others=FLAGS.others)
+    dataset = Dataset.from_pandas(df,gravity.CountryFeaturesType.ALL)
+
+    model = poisson_mixture_regression(dataset.x,2)
+    print(model.sample())
+    return 0
+
+if __name__ == '__main__':
+    app.run(main)
