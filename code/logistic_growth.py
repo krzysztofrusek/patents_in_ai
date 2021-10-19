b74e2a10c0152865a16aca8b93f615812ccbeedd
Krzysztof Rusek <krusek@agh.edu.pl>
2021-10-19 23:08:47 +0200

    eksperyemntay z e wzrostem logistycznym

--- code/logistic_growth.py
+++ code/logistic_growth.py
@@ -53,6 +53,16 @@ class LogisticGrowth(NamedTuple):
     def unpack(dense:jnp.array)->'LogisticGrowth':
         return LogisticGrowth(*jnp.split(dense,3))
 
+class LogisticGrowthV2(LogisticGrowth):
+
+    def __call__(self, x):
+        f =  jax.jit(jax.vmap(jax.grad(super(LogisticGrowthV2, self).__call__)))
+        return f(x)
+
+    def logistic_integral(self,x):
+        return super(LogisticGrowthV2, self).__call__(x)
+
+
 @jax.jit
 def fit(events:jnp.array,initial:LogisticGrowth,t0:float, T:float):
     def nll(theta:jnp.array)->jnp.array:
@@ -66,39 +76,79 @@ def fit(events:jnp.array,initial:LogisticGrowth,t0:float, T:float):
                         options=dict(maxiter=500000, line_search_maxiter=1000)
                         )
 
+@jax.jit
+def fit2(events:jnp.array,initial:LogisticGrowthV2,t0:float, T:float):
+    def nll(theta:jnp.array)->jnp.array:
+        m = LogisticGrowthV2.unpack(theta)
+        ll = jnp.sum(jnp.log(m(events))) - jnp.sum(m.logistic_integral(T)-m.logistic_integral(t0)) # remove dim of size 1
+        return -ll/1e6
+
+    return opt.minimize(nll,
+                        x0=initial.pack(),
+                        method="BFGS",
+                        options=dict(maxiter=500000, line_search_maxiter=1000)
+                        )
+
 
 def main(_):
 
     clean_df = data.load_clean(FLAGS.pickle)
-    day_events = clean_df.application_date.sort_values().to_numpy().astype('datetime64[D]')
+    day_events = clean_df.publication_date.sort_values().to_numpy().astype('datetime64[D]')
+    #day_events = day_events[50:]
     day_events = day_events[50:]
     events=day_events.astype(np.float64)
     #events = events[:-2000]/1000
-    events = events/1000
+    events = events[5000:]/1000
     t0 = events[0]
     T = events[-1]
 
     #events = (events - t0)/(T-t0)
 
-    model =LogisticGrowth(
+    model =LogisticGrowthV2(
         L=15.,
         k=1.5,
         x0=18.
     )
-    opt_result = fit(events,initial=model,t0=t0,T=T)
-    fited = LogisticGrowth.unpack(opt_result.x)
+    model =LogisticGrowthV2(
+        L=6365.,
+        k=0.46,
+        x0=34.42
+    )
+    opt_result = fit2(events,initial=model,t0=t0,T=T)
+    fited = LogisticGrowthV2.unpack(opt_result.x)
+    fited=LogisticGrowthV2(*map(lambda x: float(x),fited))
 
 
 
 
-    slambda = (pd.DataFrame(np.diff(events),index=day_events[:-1] ).ewm(alpha=0.5).mean())
+    slambda = 1/(pd.DataFrame(np.diff(events),index=day_events[:-1] ).ewm(alpha=0.02).mean())
     slambda.plot()
     _lambda = fited(events)
     plt.plot(day_events,1/_lambda)
     plt.yscale('log')
+    #plt.plot(day_events, np.cumsum(np.ones(day_events.shape)))
 
     plt.show()
 
+    plt.plot(day_events, np.cumsum(np.ones(day_events.shape)))
+    cum_lambda = model.logistic_integral(events)
+    plt.plot(day_events, cum_lambda)
+    plt.show()
+
+    slambda = 1 / (pd.DataFrame(np.diff(events), index=day_events[:-1]).ewm(alpha=0.02).mean())
+    slambda.plot()
+    plt.plot(day_events, np.cumsum(np.ones(day_events.shape)))
+    cum_lambda = fited.logistic_integral(events)
+    plt.plot(day_events, cum_lambda)
+    cum_lambda = fited(events)
+    plt.plot(day_events, cum_lambda)
+    plt.show()
+
+    cum_lambda = fited.logistic_integral(events)
+    plt.plot(day_events, cum_lambda)
+    cum_lambda = fited(events)
+    plt.plot(day_events, cum_lambda)
+    plt.show()
 
 
 
