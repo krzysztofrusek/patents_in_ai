35eb2ad937f173abe1250a8458f66ef18b020390
Krzysztof Rusek <krusek@agh.edu.pl>
2021-11-18 17:06:52 +0100

    Bayesowski trend - Moduły

--- code/logistic_growth.py
+++ code/logistic_growth.py
@@ -1,3 +1,5 @@
+import functools
+
 from jax.config import config
 config.update("jax_enable_x64", True)
 
@@ -13,107 +15,181 @@ from typing import NamedTuple
 from absl import flags, app, logging
 from tensorflow_probability.substrates import jax as tfp
 tfd = tfp.distributions
-
+import haiku as hk
 import data
 
 flags.DEFINE_string("pickle", "../dane/clean.pickle", "Input file")
 
 FLAGS= flags.FLAGS
 
+#%%
+# https://github.com/deepmind/deepmind-research/blob/master/counterfactual_fairness/causal_network.py
+
+class NormalPosterior(hk.Module):
+    def __init__(self,prior:tfd.Distribution,num_kl=1,name=None):
+        super().__init__(name=name)
+
+        self.prior = prior
+        self.num_kl = num_kl
+
+    def __call__(self):
+        loc = hk.get_parameter('loc', shape=self.prior.event_shape, init=jnp.zeros)
+        log_var = hk.get_parameter('log_var', shape=self.prior.event_shape, init=jnp.ones)
+        scale = jnp.sqrt(jnp.exp(log_var))
+        posterior = tfd.Normal(loc=loc, scale=scale)
+        param = posterior.sample(self.num_kl, seed=hk.next_rng_key())
+        hk.set_state('kl',self.prior.log_prob(param))
+        return param
 
 
-class LogisticGrowth(NamedTuple):
-    '''
-    https://en.wikipedia.org/wiki/Logistic_function
-
-    '''
-    L:jnp.array
-    k:jnp.array
-    x0:jnp.array
-
-    @property
-    def _L(self):
-        return 1000.*self.L
-        #return  self.L
-    def __call__(self, x:jnp.array)->jnp.array:
-        return self._L/(1+ jnp.exp(-self.k*(x-self.x0)))
-
-    def logistic_integral(self,x):
-        '''
-        https://www.wolframalpha.com/input/?i=integrate+L%2F%281%2Be%5E%28-k+%28x-x0%29%29%29
-        :param x:
-        :return:
-        '''
-        #integral = self.L*jnp.log(jnp.exp(self.k*(x-self.x0))+1)/self.k + self.L * x
-        integral = self._L * jax.nn.softplus(self.k * (self.x0-x)) / self.k + self._L * x
-        return integral
-
-    def pack(self):
-        return jnp.stack(self)
-
-    @staticmethod
-    def unpack(dense:jnp.array)->'LogisticGrowth':
-        return LogisticGrowth(*jnp.split(dense,3))
-
-class LogisticGrowthV2(LogisticGrowth):
-
-    def __call__(self, x):
-        f =  jax.jit(jax.vmap(jax.grad(super(LogisticGrowthV2, self).__call__)))
-        return f(x)
-
-    def logistic_integral(self,x):
-        return super(LogisticGrowthV2, self).__call__(x)
-
-
-@jax.jit
-def fit(events:jnp.array,initial:LogisticGrowth,t0:float, T:float):
-    def nll(theta:jnp.array)->jnp.array:
-        m = LogisticGrowth.unpack(theta)
-        ll = jnp.sum(jnp.log(m(events))) - jnp.sum(m.logistic_integral(T)-m.logistic_integral(t0)) # remove dim of size 1
-        return -ll/1e4
-
-    return opt.minimize(nll,
-                        x0=initial.pack(),
-                        method="BFGS",
-                        options=dict(maxiter=500000, line_search_maxiter=1000)
-                        )
-
-@jax.jit
-def fit2(events:jnp.array,initial:LogisticGrowthV2,t0:float, T:float):
-    def nll(theta:jnp.array)->jnp.array:
-        m = LogisticGrowthV2.unpack(theta)
-        ll = jnp.sum(jnp.log(m(events))) - jnp.sum(m.logistic_integral(T)-m.logistic_integral(t0)) # remove dim of size 1
-        return -ll/1e6
-
-    return opt.minimize(nll,
-                        x0=initial.pack(),
-                        method="BFGS",
-                        options=dict(maxiter=500000, line_search_maxiter=1000)
-                        )
-
-
-class LogisticGrowthV3(NamedTuple):
-    '''
-    https://en.wikipedia.org/wiki/Logistic_function
-
-    '''
-    a:jnp.array
-    loc:jnp.array
-    scale:jnp.array
-    mix:jnp.array
+class InhomogeneousPoissonProcess(NamedTuple):
+    maximum:jnp.ndarray
+    midpoints:jnp.ndarray
+    rates:jnp.ndarray
+    mix:jnp.ndarray
 
     @property
-    def dist(self):
-        dist = tfd.MixtureSameFamily(
+    def distribution(self):
+        return tfd.MixtureSameFamily(
             mixture_distribution=tfd.Categorical(logits=self.mix),
-            components_distribution=tfd.Logistic(loc=self.loc, scale=self.scale)
+            components_distribution=tfd.Logistic(loc=self.midpoints, scale=self.rates)
         )
-        return dist
+
+    def log_rate(self,events:jnp.ndarray):
+        return jnp.log(self.maximum)+ self.distribution.log_prob(events)
+
+    def cumulative_rate(self,x:jnp.ndarray):
+        return self.maximum * self.distribution.cdf(x)
+
+    @functools.partial(jax.vmap, in_axes=(0,None))
+    def log_prob(self,events:jnp.ndarray):
+        return jnp.sum(self.log_rate(events)) - \
+               (self.cumulative_rate(events[-1]) - self.cumulative_rate(events[0]))
 
 
-    def __call__(self, x:jnp.array)->jnp.array:
+class LogisticGrowthSuperposition(hk.Module):
+    def __init__(self, num_kl:int=4,name=None):
+        super().__init__(name=name)
 
-        return self.a*self.dist.cdf(x)
+        self.maximum = NormalPosterior(
+            prior=tfd.LogNormal(loc=1., scale=1.),num_kl=num_kl,
+            name = 'maximum'
+        )
+        self.midpoints= NormalPosterior(
+            prior=tfd.Sample( tfd.Normal(1,0.5),2 ),num_kl=num_kl,
+            name='midpoints'
+        )
+        self.rates = NormalPosterior(
+            prior=tfd.Sample( tfd.Normal(1,0.5),2 ),num_kl=num_kl,
+            name='rates'
+        )
+        self.mix = NormalPosterior(
+            prior=tfd.Sample( tfd.Normal(0,5.),2 ),num_kl=num_kl,
+            name='mix'
+        )
+
+    def __call__(self):
+
+        return InhomogeneousPoissonProcess(
+            maximum=self.maximum(),
+            midpoints=self.midpoints(),
+            rates=self.rates(),
+            mix = self.mix()
+        )
+
+#
+# class LogisticGrowth(NamedTuple):
+#     '''
+#     https://en.wikipedia.org/wiki/Logistic_function
+#
+#     '''
+#     L:jnp.array
+#     k:jnp.array
+#     x0:jnp.array
+#
+#     @property
+#     def _L(self):
+#         return 1000.*self.L
+#         #return  self.L
+#     def __call__(self, x:jnp.array)->jnp.array:
+#         return self._L/(1+ jnp.exp(-self.k*(x-self.x0)))
+#
+#     def logistic_integral(self,x):
+#         '''
+#         https://www.wolframalpha.com/input/?i=integrate+L%2F%281%2Be%5E%28-k+%28x-x0%29%29%29
+#         :param x:
+#         :return:
+#         '''
+#         #integral = self.L*jnp.log(jnp.exp(self.k*(x-self.x0))+1)/self.k + self.L * x
+#         integral = self._L * jax.nn.softplus(self.k * (self.x0-x)) / self.k + self._L * x
+#         return integral
+#
+#     def pack(self):
+#         return jnp.stack(self)
+#
+#     @staticmethod
+#     def unpack(dense:jnp.array)->'LogisticGrowth':
+#         return LogisticGrowth(*jnp.split(dense,3))
+#
+# class LogisticGrowthV2(LogisticGrowth):
+#
+#     def __call__(self, x):
+#         f =  jax.jit(jax.vmap(jax.grad(super(LogisticGrowthV2, self).__call__)))
+#         return f(x)
+#
+#     def logistic_integral(self,x):
+#         return super(LogisticGrowthV2, self).__call__(x)
+#
+#
+# @jax.jit
+# def fit(events:jnp.array,initial:LogisticGrowth,t0:float, T:float):
+#     def nll(theta:jnp.array)->jnp.array:
+#         m = LogisticGrowth.unpack(theta)
+#         ll = jnp.sum(jnp.log(m(events))) - jnp.sum(m.logistic_integral(T)-m.logistic_integral(t0)) # remove dim of size 1
+#         return -ll/1e4
+#
+#     return opt.minimize(nll,
+#                         x0=initial.pack(),
+#                         method="BFGS",
+#                         options=dict(maxiter=500000, line_search_maxiter=1000)
+#                         )
+#
+# @jax.jit
+# def fit2(events:jnp.array,initial:LogisticGrowthV2,t0:float, T:float):
+#     def nll(theta:jnp.array)->jnp.array:
+#         m = LogisticGrowthV2.unpack(theta)
+#         ll = jnp.sum(jnp.log(m(events))) - jnp.sum(m.logistic_integral(T)-m.logistic_integral(t0)) # remove dim of size 1
+#         return -ll/1e6
+#
+#     return opt.minimize(nll,
+#                         x0=initial.pack(),
+#                         method="BFGS",
+#                         options=dict(maxiter=500000, line_search_maxiter=1000)
+#                         )
+#
+#
+# class LogisticGrowthV3(NamedTuple):
+#     '''
+#     https://en.wikipedia.org/wiki/Logistic_function
+#
+#     '''
+#     a:jnp.array
+#     loc:jnp.array
+#     scale:jnp.array
+#     mix:jnp.array
+#
+#     @property
+#     def dist(self):
+#         dist = tfd.MixtureSameFamily(
+#             mixture_distribution=tfd.Categorical(logits=self.mix),
+#             components_distribution=tfd.Logistic(loc=self.loc, scale=self.scale)
+#         )
+#         return dist
+#
+#
+#     def __call__(self, x:jnp.array)->jnp.array:
+#
+#         return self.a*self.dist.cdf(x)
 
 def main(_):
 
---
+++ code/logistic_growth_test.py
@@ -0,0 +1,78 @@
+import functools
+import unittest
+from typing import NamedTuple
+import functools
+
+import logistic_growth as lg
+import haiku as hk
+import jax
+import jax.numpy as jnp
+tfd = lg.tfd
+
+
+class PosteriorTestCase(unittest.TestCase):
+    def test_trainable(self):
+
+        @hk.transform_with_state
+        def trainable_normal():
+            return lg.NormalPosterior(prior=tfd.Normal(loc=0., scale=2.),name="test")()
+
+        rng = jax.random.PRNGKey(42)
+        trainable_normal.init(rng)
+
+
+class IPPTestCase(unittest.TestCase):
+    def test_vmap(self):
+        dist = lg.InhomogeneousPoissonProcess(
+            maximum=jnp.array(2*[4.]),
+            rates=jnp.ones((2,2)),
+            midpoints= jnp.ones((2, 2)),
+            mix = jnp.zeros((2,2))
+        )
+        t = jnp.array([1, 2, 3])
+
+        dist.log_prob(t)
+
+    # def test_vmap_distrax(self):
+    #     d = distrax.Normal(loc=jnp.array([0.,1,]), scale=jnp.array([2.,1,]))
+    #     #d = tfd.Normal(loc=jnp.array([0.,1,]), scale=jnp.array([2.,1,]))
+    #     d = distrax.Normal(loc=jnp.array([0. ]), scale=jnp.array([2.]))
+    #
+    #     x = jnp.array([1.,2,3])
+    #
+    #     def _lp(dist, x):
+    #         return dist.log_prob(x)
+    #     lp = jax.vmap(_lp, in_axes=(0,None),out_axes=(2))
+    #
+    #     lp(d,x)
+    #
+    #     class TMP(NamedTuple):
+    #         a:jnp.ndarray
+    #
+    #         @functools.partial(jax.vmap,in_axes=(0,None))
+    #         def f(self,x):
+    #             #return self.a+x
+    #             return distrax.Normal(loc=self.a, scale=2.).log_prob(x)
+    #
+    #     tmp=TMP(a=jnp.array([1,2]))
+    #
+    #     tmp.f(jnp.array([1,2,3]))
+
+class LogisticGrowthTestCase(unittest.TestCase):
+    def test_model(self):
+
+        @hk.transform_with_state
+        def model():
+            m = lg.LogisticGrowthSuperposition()
+            return m()
+
+        rng = jax.random.PRNGKey(42)
+        params, state = model.init(rng)
+        dist, state = model.apply(params, state,rng)
+        t = jnp.array([1, 2, 3.])
+        dist.log_prob(t)
+        ...
+
+
+if __name__ == '__main__':
+    unittest.main()
